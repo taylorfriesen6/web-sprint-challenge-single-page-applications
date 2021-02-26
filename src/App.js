99ee68ee0c6dc404cc24faede20289f6b5d2b21c
Taylor Friesen <taylorfriesen@protonmail.com>
2021-02-26 08:43:45 -0800

    routes created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <h1>Lambda Eats</h1>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/pizza'>Pizza</Link>
+      </nav>
       <p>You can remove this code and create your own header</p>
-    </>
+      <Switch>
+        <Route path='/pizza'>pizza</Route>
+        <Route path='/'>home</Route>
+      </Switch>
+    </Router>
   );
 };
 export default App;
