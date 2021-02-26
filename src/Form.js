a863eb878df57f769f770cd7261c4782816918a7
Taylor Friesen <taylorfriesen@protonmail.com>
2021-02-26 09:23:57 -0800

    controlled input for all form elements

--- src/Form.js
+++ src/Form.js
@@ -3,20 +3,53 @@ import React, {useState, useEffect} from "react";
 export default function Form (props) {
   const initialState = {
     name: '',
+    size: '',
+    pepperoni: false,
+    mushrooms: false,
+    peppers: false,
+    pineapple: false,
   };
 
   const handleChange = (e) => {
-    const {name, value} = e.target;
-    setForm({...form, [name]: value});
+    const { name, type, value, checked } = e.target;
+    const updatedInfo = type === 'checkbox' ? checked : value;
+    setForm({ ...form, [name]: updatedInfo });
   }
 
 
   const [form, setForm] = useState(initialState);
   return (
     <form>
-      <label>
-        Name: <input type='text' name='name' value={form.name} onChange={handleChange}/>
-      </label>
+      <p>
+        <label>
+          Name: <input type='text' name='name' value={form.name} onChange={handleChange}/>
+        </label>
+      </p>
+      <p>
+        <label>
+          Pizza size:
+          <select name='size' value={form.size} onChange={handleChange}>
+            <option value='small'>Small</option>
+            <option value='medium'>Medium</option>
+            <option value='large'>Large</option>
+          </select>
+        </label>
+      </p>
+      <p>
+        Toppings:
+        <label>
+          <input type='checkbox' name='pepperoni' checked={form.pepperoni} onChange={handleChange}/>Pepperoni
+        </label>
+        <label>
+          <input type='checkbox' name='mushrooms' checked={form.mushrooms} onChange={handleChange}/>Mushrooms
+        </label>
+        <label>
+          <input type='checkbox' name='peppers' checked={form.peppers} onChange={handleChange}/>Peppers
+        </label>
+        <label>
+          <input type='checkbox' name='pineapple' checked={form.pineapple} onChange={handleChange}/>Pineapple
+        </label>
+      </p>
     </form>
   );
 }
